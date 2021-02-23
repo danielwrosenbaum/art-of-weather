a7218b7da53759a2e0c205b961c5cdc1e7945c04
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-23 07:06:01 +0000

    Ensure that painting title, artist and year is included underneath painting & Add input box and text. #1

--- js/main.js
+++ js/main.js
@@ -25,3 +25,23 @@ function getArtData(weather) {
 }
 
 getArtData('autumn');
+
+function getWeather(cityName) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=8e56c099331856fb966227282999fa5c');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    // console.log(xhr.status);
+    // console.log(xhr.response);
+    // var weatherCondition = xhr.response.weather[0].main;
+    // var newCityTemp = document.createElement('div');
+    // newCityTemp.textContent = cityName + ' ' + 'current temperature:' + ' ' + xhr.response.main.temp;
+    // $newWeather.appendChild(newCityTemp);
+    // var newCityWeather = document.createElement('div');
+    // newCityWeather.textContent = cityName + ' ' + 'current conditions:' + ' ' + weatherCondition;
+    // $newWeather.appendChild(newCityWeather);
+  });
+  xhr.send();
+}
+
+getWeather('chicago');
