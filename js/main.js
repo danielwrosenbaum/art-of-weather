67b2eca7f0eb2fd5e4e9e36da1664ed00b2cc1af
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-24 19:27:06 +0000

    Separated and named all functions. #3

--- js/main.js
+++ js/main.js
@@ -49,46 +49,54 @@ function goToView(event) {
 
 }
 function changeBackground(weather) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://www.rijksmuseum.nl/api/en/collection?key=TnIr6Ed8&imgonly=true&type=painting&q=' + weather);
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
-    var i = Math.floor(Math.random() * xhr.response.artObjects.length);
-    var $newPic = xhr.response.artObjects[i].webImage.url;
-    var $newPicAlt = xhr.response.artObjects[i].title;
-    $randomBackground.setAttribute('src', $newPic);
-    $randomBackground.setAttribute('alt', $newPicAlt);
-    $backgroundPic.prepend($randomBackground);
+  var xhr4 = new XMLHttpRequest();
+  xhr4.open('GET', 'https://www.rijksmuseum.nl/api/en/collection?key=TnIr6Ed8&imgonly=true&type=painting&q=' + weather);
+  xhr4.responseType = 'json';
+  xhr4.addEventListener('load', function () {
+    generateRandomBackground(xhr4.response);
   });
-  xhr.send();
+  xhr4.send();
+}
+
+function generateRandomBackground(response) {
+  var i = Math.floor(Math.random() * response.artObjects.length);
+  var $newPic = response.artObjects[i].webImage.url;
+  var $newPicAlt = response.artObjects[i].title;
+  $randomBackground.setAttribute('src', $newPic);
+  $randomBackground.setAttribute('alt', $newPicAlt);
+  $backgroundPic.prepend($randomBackground);
 }
 
 function getArtData(weather) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://www.rijksmuseum.nl/api/en/collection?key=TnIr6Ed8&imgonly=true&type=painting&q=' + weather);
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
-    var i = Math.floor(Math.random() * xhr.response.artObjects.length);
-    var $newPic = xhr.response.artObjects[i].webImage.url;
-    var $newPicAlt = xhr.response.artObjects[i].title;
-    $randomBackground.setAttribute('src', $newPic);
-    $randomBackground.setAttribute('alt', $newPicAlt);
-    $backgroundPic.prepend($randomBackground);
-    $imageContainer.className = 'image-container';
-    $getWeatherPage.prepend($imageContainer);
-    var $newImage = document.createElement('img');
-    $newImage.setAttribute('src', $newPic);
-    $newImage.setAttribute('alt', $newPicAlt);
-    $newImage.className = 'main-pic';
-    $imageContainer.prepend($newImage);
-    var $newImageTitle = document.createElement('h4');
-    $newImageTitle.textContent = xhr.response.artObjects[i].title;
-    $imageContainer.appendChild($newImageTitle);
-    var $newImageArtist = document.createElement('div');
-    $newImageArtist.textContent = xhr.response.artObjects[i].principalOrFirstMaker;
-    $imageContainer.appendChild($newImageArtist);
+  var xhr3 = new XMLHttpRequest();
+  xhr3.open('GET', 'https://www.rijksmuseum.nl/api/en/collection?key=TnIr6Ed8&imgonly=true&type=painting&q=' + weather);
+  xhr3.responseType = 'json';
+  xhr3.addEventListener('load', function () {
+    generateGetWeatherPage(xhr3.response);
   });
-  xhr.send();
+  xhr3.send();
+}
+
+function generateGetWeatherPage(response) {
+  var i = Math.floor(Math.random() * response.artObjects.length);
+  var $newPic = response.artObjects[i].webImage.url;
+  var $newPicAlt = response.artObjects[i].title;
+  $randomBackground.setAttribute('src', $newPic);
+  $randomBackground.setAttribute('alt', $newPicAlt);
+  $backgroundPic.prepend($randomBackground);
+  $imageContainer.className = 'image-container';
+  $getWeatherPage.prepend($imageContainer);
+  var $newImage = document.createElement('img');
+  $newImage.setAttribute('src', $newPic);
+  $newImage.setAttribute('alt', $newPicAlt);
+  $newImage.className = 'main-pic';
+  $imageContainer.prepend($newImage);
+  var $newImageTitle = document.createElement('h4');
+  $newImageTitle.textContent = response.artObjects[i].title;
+  $imageContainer.appendChild($newImageTitle);
+  var $newImageArtist = document.createElement('div');
+  $newImageArtist.textContent = response.artObjects[i].principalOrFirstMaker;
+  $imageContainer.appendChild($newImageArtist);
 }
 
 getArtData('snow');
@@ -113,66 +121,74 @@ function getWeather(cityName) {
   xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=8e56c099331856fb966227282999fa5c');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    if (xhr.response.cod === '404') {
-      $weatherPage.prepend($errorContainer);
-      var notFound = document.createElement('h1');
-      notFound.className = 'text-align';
-      notFound.textContent = 'City not found, please try again.';
-      $errorContainer.appendChild(notFound);
-    } else {
-      var cityTemp = Math.trunc(xhr.response.main.temp);
-      var weatherCondition = xhr.response.weather[0].main;
-      $mainHeading.className = 'heading hidden';
-      $weatherHeading.textContent = weatherCondition + '  ' + cityTemp + ' \u00B0F';
-      $headingContainer.appendChild($weatherHeading);
-      getArtWeather(weatherCondition);
-      $weatherContainer.className = 'weather-container';
-      $weatherPage.appendChild($weatherContainer);
-      var $cityName = document.createElement('h1');
-      $cityName.textContent = xhr.response.name;
-      $weatherContainer.append($cityName);
-      var newCityTemp = document.createElement('div');
-      newCityTemp.textContent = cityTemp + ' \u00B0F';
-      $weatherContainer.appendChild(newCityTemp);
-      var newCityWeather = document.createElement('div');
-      newCityWeather.textContent = weatherCondition;
-      $weatherContainer.appendChild(newCityWeather);
-    }
-    $weatherPage.appendChild($backButtonContainer);
-    $backButtonContainer.className = 'button-container';
-    $weatherBackButton.className = 'back-button';
-    $weatherBackButton.textContent = 'Back';
-    $backButtonContainer.appendChild($weatherBackButton);
-    $form.reset();
+    generateWeatherContent(xhr.response);
   });
   xhr.send();
 }
 
+function generateWeatherContent(response) {
+  if (response.cod === '404') {
+    $weatherPage.prepend($errorContainer);
+    var notFound = document.createElement('h1');
+    notFound.className = 'text-align';
+    notFound.textContent = 'City not found, please try again.';
+    $errorContainer.appendChild(notFound);
+  } else {
+    var cityTemp = Math.trunc(response.main.temp);
+    var weatherCondition = response.weather[0].main;
+    $mainHeading.className = 'heading hidden';
+    $weatherHeading.textContent = weatherCondition + '  ' + cityTemp + ' \u00B0F';
+    $headingContainer.appendChild($weatherHeading);
+    getArtWeather(weatherCondition);
+    $weatherContainer.className = 'weather-container';
+    $weatherPage.appendChild($weatherContainer);
+    var $cityName = document.createElement('h1');
+    $cityName.textContent = response.name;
+    $weatherContainer.append($cityName);
+    var newCityTemp = document.createElement('div');
+    newCityTemp.textContent = cityTemp + ' \u00B0F';
+    $weatherContainer.appendChild(newCityTemp);
+    var newCityWeather = document.createElement('div');
+    newCityWeather.textContent = weatherCondition;
+    $weatherContainer.appendChild(newCityWeather);
+  }
+  $weatherPage.appendChild($backButtonContainer);
+  $backButtonContainer.className = 'button-container';
+  $weatherBackButton.className = 'back-button';
+  $weatherBackButton.textContent = 'Back';
+  $backButtonContainer.appendChild($weatherBackButton);
+  $form.reset();
+}
+
 function getArtWeather(weather) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://www.rijksmuseum.nl/api/en/collection?key=TnIr6Ed8&imgonly=true&type=painting&q=' + weather);
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
-    var i = Math.floor(Math.random() * xhr.response.artObjects.length);
-    var $newPic = xhr.response.artObjects[i].webImage.url;
-    var $newPicAlt = xhr.response.artObjects[i].title;
-    $randomBackground.setAttribute('src', $newPic);
-    $randomBackground.setAttribute('alt', $newPicAlt);
-    $backgroundPic.prepend($randomBackground);
-    var $imageContainer = document.createElement('div');
-    $imageContainer.className = 'image-container';
-    $weatherPage.prepend($imageContainer);
-    var $newImage = document.createElement('img');
-    $newImage.setAttribute('src', $newPic);
-    $newImage.setAttribute('alt', $newPicAlt);
-    $newImage.className = 'main-pic';
-    $imageContainer.prepend($newImage);
-    var $newImageTitle = document.createElement('h4');
-    $newImageTitle.textContent = xhr.response.artObjects[i].title;
-    $imageContainer.appendChild($newImageTitle);
-    var $newImageArtist = document.createElement('div');
-    $newImageArtist.textContent = xhr.response.artObjects[i].principalOrFirstMaker;
-    $imageContainer.appendChild($newImageArtist);
+  var xhr2 = new XMLHttpRequest();
+  xhr2.open('GET', 'https://www.rijksmuseum.nl/api/en/collection?key=TnIr6Ed8&imgonly=true&type=painting&q=' + weather);
+  xhr2.responseType = 'json';
+  xhr2.addEventListener('load', function () {
+    generateWeatherPicture(xhr2.response);
   });
-  xhr.send();
+  xhr2.send();
+}
+
+function generateWeatherPicture(response) {
+  var i = Math.floor(Math.random() * response.artObjects.length);
+  var $newPic = response.artObjects[i].webImage.url;
+  var $newPicAlt = response.artObjects[i].title;
+  $randomBackground.setAttribute('src', $newPic);
+  $randomBackground.setAttribute('alt', $newPicAlt);
+  $backgroundPic.prepend($randomBackground);
+  var $imageContainer = document.createElement('div');
+  $imageContainer.className = 'image-container';
+  $weatherPage.prepend($imageContainer);
+  var $newImage = document.createElement('img');
+  $newImage.setAttribute('src', $newPic);
+  $newImage.setAttribute('alt', $newPicAlt);
+  $newImage.className = 'main-pic';
+  $imageContainer.prepend($newImage);
+  var $newImageTitle = document.createElement('h4');
+  $newImageTitle.textContent = response.artObjects[i].title;
+  $imageContainer.appendChild($newImageTitle);
+  var $newImageArtist = document.createElement('div');
+  $newImageArtist.textContent = response.artObjects[i].principalOrFirstMaker;
+  $imageContainer.appendChild($newImageArtist);
 }
