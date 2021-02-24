51224e78ff4e3a09cc2c59984e39d738e6d0c6bb
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-24 03:53:56 +0000

    have header change to reflect the current conditions. #3

--- js/main.js
+++ js/main.js
@@ -84,27 +84,56 @@ function getWeather(cityName) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=8e56c099331856fb966227282999fa5c');
   xhr.responseType = 'json';
-
   xhr.addEventListener('load', function () {
     // console.log(xhr.response);
     var weatherCondition = xhr.response.weather[0].main;
-    var newCityTemp = document.createElement('div');
-    newCityTemp.textContent = cityName + ' ' + 'current temperature:' + ' ' + xhr.response.main.temp;
-    $weatherPage.appendChild(newCityTemp);
-    var newCityWeather = document.createElement('div');
-    newCityWeather.textContent = cityName + ' ' + 'current conditions:' + ' ' + weatherCondition;
-    $weatherPage.appendChild(newCityWeather);
     $mainHeading.className = 'heading hidden';
     var $weatherHeading = document.createElement('h1');
     $weatherHeading.textContent = weatherCondition;
     $headingContainer.appendChild($weatherHeading);
+    getArtWeather(weatherCondition);
     var $weatherContainer = document.createElement('div');
     $weatherContainer.className = 'weather-container';
     $weatherPage.appendChild($weatherContainer);
     var $cityName = document.createElement('h1');
     $cityName.textContent = xhr.response.name;
     $weatherContainer.prepend($cityName);
+    var newCityTemp = document.createElement('div');
+    newCityTemp.textContent = xhr.response.main.temp + ' \u00B0F';
+    $weatherPage.appendChild(newCityTemp);
+    var newCityWeather = document.createElement('div');
+    newCityWeather.textContent = weatherCondition;
+    $weatherPage.appendChild(newCityWeather);
 
+  });
+  xhr.send();
+}
+
+function getArtWeather(weather) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://www.rijksmuseum.nl/api/en/collection?key=TnIr6Ed8&imgonly=true&type=painting&q=' + weather);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    var i = Math.floor(Math.random() * xhr.response.artObjects.length);
+    var $newPic = xhr.response.artObjects[i].webImage.url;
+    var $newPicAlt = xhr.response.artObjects[i].title;
+    $randomBackground.setAttribute('src', $newPic);
+    $randomBackground.setAttribute('alt', $newPicAlt);
+    $backgroundPic.prepend($randomBackground);
+    var $imageContainer = document.createElement('div');
+    $imageContainer.className = 'image-container';
+    $weatherPage.prepend($imageContainer);
+    var $newImage = document.createElement('img');
+    $newImage.setAttribute('src', $newPic);
+    $newImage.setAttribute('alt', $newPicAlt);
+    $newImage.className = 'main-pic';
+    $imageContainer.prepend($newImage);
+    var $newImageTitle = document.createElement('h4');
+    $newImageTitle.textContent = xhr.response.artObjects[i].title;
+    $imageContainer.appendChild($newImageTitle);
+    var $newImageArtist = document.createElement('div');
+    $newImageArtist.textContent = xhr.response.artObjects[i].principalOrFirstMaker;
+    $imageContainer.appendChild($newImageArtist);
   });
   xhr.send();
 }
